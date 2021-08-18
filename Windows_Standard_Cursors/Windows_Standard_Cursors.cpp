d431a5fadf8a7c1569bf111c1ec99158116e3abd
Lamed(라메드) <53391835+Lamed-D@users.noreply.github.com>
2021-08-18 19:42:48 +0900

    SIZE CHANGE

--- Windows_Standard_Cursors/Windows_Standard_Cursors.cpp
+++ Windows_Standard_Cursors/Windows_Standard_Cursors.cpp
@@ -95,20 +95,21 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+    //윈도우 사이즈 조절 (코드 개발한 부분)
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, 440, 550, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+    if (!hWnd)
+    {
+        return FALSE;
+    }
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
 
-   return TRUE;
+    return TRUE;
 }
 
 //
@@ -121,16 +122,88 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
 //
 //
+
+//버튼문자열 (코드 개발한 부분)
+char IDC_CURSORS[][20] = { "IDC_ARROW","IDC_IBEAM","IDC_WAIT","IDC_CROSS","IDC_UPARROW",
+                            "IDC_SIZE","IDC_ICON","IDC_SIZENWSE","IDC_SIZENESW","IDC_SIZEWE",
+                            "IDC_SIZENS","IDC_SIZEALL","IDC_NO","IDC_HAND","IDC_APPSTARTING",
+                            "IDC_HELP","IDC_PIN","IDC_PERSON" };
+//윈도우 창의 커서 (코드 개발한 부분)
+HICON SET_IDC_CURSORS = LoadCursor(0, IDC_ARROW);
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    //바뀌는 커서를 바꾸기위한 무한 루틴
+    SetCursor(SET_IDC_CURSORS);
     switch (message)
     {
+    //버튼 출력(코드 개발한 부분)
+    case WM_CREATE:
+        for (int i = 0, j = 0; i < 18; i++)
+        {
+            CreateWindowA("button", IDC_CURSORS[i], WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                (20 + (j * 200)), 20 + ((i - (j * 12)) * 40), 135, 25, hWnd, (HMENU)(i + 1), hInst, NULL);
+            if (i == 11)
+                j++;
+        }
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
             // 메뉴 선택을 구문 분석합니다:
             switch (wmId)
             {
+            //버튼 기능들(코드 개발한 부분)=
+            case 1:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_ARROW);
+                break;
+            case 2:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_IBEAM);
+                break;
+            case 3:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_WAIT);
+                break;
+            case 4:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_CROSS);
+                break;
+            case 5:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_UPARROW);
+                break;
+            case 6:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_SIZE);
+                break;
+            case 7:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_ICON);
+                break;
+            case 8:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_SIZENWSE);
+                break;
+            case 9:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_SIZENESW);
+                break;
+            case 10:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_SIZEWE);
+                break;
+            case 11:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_SIZENS);
+                break;
+            case 12:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_SIZEALL);
+                break;
+            case 13:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_NO);
+                break;
+            case 14:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_HAND);
+                break;
+            case 15:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_APPSTARTING);
+                break;
+            case 16:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_PIN);
+                break;
+            case 17:
+                SET_IDC_CURSORS = LoadCursor(0, IDC_PERSON);
+                break;
             case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
                 break;
@@ -147,6 +220,41 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+            //커서 출력(코드 개발한 부분)
+
+            //실험용 코드들(무시하면 됨)
+            //HICON icon = LoadIcon(0, IDC_ARROW);
+            //HICON icon = LoadCursor(0, IDC_ARROW);
+            //DrawIcon(hdc, 10, 20, icon);
+            //ShowCursor(FALSE);
+
+            HICON IDC_CURSORS[20];
+            IDC_CURSORS[0] = LoadCursor(0, IDC_ARROW);
+            IDC_CURSORS[1] = LoadCursor(0, IDC_IBEAM);
+            IDC_CURSORS[2] = LoadCursor(0, IDC_WAIT);
+            IDC_CURSORS[3] = LoadCursor(0, IDC_CROSS);
+            IDC_CURSORS[4] = LoadCursor(0, IDC_UPARROW);
+            IDC_CURSORS[5] = LoadCursor(0, IDC_SIZE);
+            IDC_CURSORS[6] = LoadCursor(0, IDC_ICON);
+            IDC_CURSORS[7] = LoadCursor(0, IDC_SIZENWSE);
+            IDC_CURSORS[8] = LoadCursor(0, IDC_SIZENESW);
+            IDC_CURSORS[9] = LoadCursor(0, IDC_SIZEWE);
+            IDC_CURSORS[10] = LoadCursor(0, IDC_SIZENS);
+            IDC_CURSORS[11] = LoadCursor(0, IDC_SIZEALL);
+            IDC_CURSORS[12] = LoadCursor(0, IDC_NO);
+            IDC_CURSORS[13] = LoadCursor(0, IDC_HAND);
+            IDC_CURSORS[14] = LoadCursor(0, IDC_APPSTARTING);
+            IDC_CURSORS[15] = LoadCursor(0, IDC_HELP);
+            IDC_CURSORS[16] = LoadCursor(0, IDC_PIN);
+            IDC_CURSORS[17] = LoadCursor(0, IDC_PERSON);
+
+            for (int i = 0, j = 0; i < 18; i++)
+            {
+                DrawIcon(hdc, (165 + (j * 200)), 20 + ((i - (j * 12)) * 40), IDC_CURSORS[i]);
+                if (i == 11)
+                    j++;
+            }
             EndPaint(hWnd, &ps);
         }
         break;
